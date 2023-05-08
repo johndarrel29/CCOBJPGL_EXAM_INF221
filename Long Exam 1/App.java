d96395636915f872686aac9ea83eb31eb3dd01ae
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-05-08 14:33:54 +0800

    Update App.java

--- Long Exam 1/App.java
+++ Long Exam 1/App.java
@@ -25,9 +25,5 @@ public class App {
         Subic.accept(rere);
         StrawberryFarm.accept(rere);
         hundredIslands.accept(rere);
-        
-        //Single dispatch
-        rere.visit(batangas);
-
     }
 }
